53ee8a91f092523aac849aeaf15a51f9e54e8339
Askhalan A <askhalan.aj@gmail.com>
2024-06-21 15:00:49 +0530

    h

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,78 @@
+
+
+import 'package:flutter/material.dart';
+
+class JColor {
+  JColor._();
+
+// ===================================Project-Specific-Colours======================================
+
+  // App Basic Colors
+  static const Color primary =Color(0xFFFFFFFF);
+  static const Color secondary = Color(0xFFFFFFFF);
+  static const Color accent =Color(0xFFFFFFFF);
+
+  // Background Colors
+  static const Color light = Color(0xFFF6F6F6);
+  static const Color dark = Color(0xFF272727);
+  static const Color bg = Color(0xFFFFFFFF);
+  static const Color bgSecondary = Color(0xFFFFFFFF);
+
+  // Icon Color
+  static const Color icon = Color(0xFF797979);
+  static const Color iconAccent = Color(0xFFFFFFFF);
+
+  // Text colors
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color ttextSecondary = Color(0xFFFFFFFF);
+  static const Color textWhite = Colors.white;
+
+  // Button colors
+  static const Color buttonPrimary =Color(0xFFFFFFFF);
+  static const Color buttonSecondary = Color(0xFFFFFFFF);
+  static const Color buttonDisabled = Color(0xFFC4C4C4);
+
+// ======================================Constant-Colors===========================================
+
+  // Border colors
+  static const Color borderPrimary = Color(0xFFD9D9D9);
+  static const Color borderSecondary = Color(0xFFE6E6E6);
+
+  // Error and validation colors
+  static const Color error = Color.fromARGB(255, 255, 77, 77);
+  static const Color success = Color(0xFF388E3C);
+  static const Color warning = Color(0xFFF57C00);
+  static const Color info = Color(0xFF1976D2);
+
+  // Neutral Shades
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFE0E0E0);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color lightGrey = Color(0xFFF9F9F9);
+  static const Color white = Color(0xFFFFFFFF);
+}
+
+
+
+// ======================================Gradient Colors===========================================
+
+ const Gradient gradientBg = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      Color.fromARGB(255, 255, 179, 86),
+      Color.fromARGB(255, 240, 199, 133),
+      Color.fromARGB(255, 255, 179, 86),
+    ],
+  );
+  const Gradient gradientBgTile = LinearGradient(
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+    colors: [
+      Color.fromARGB(255, 248, 186, 111),
+      Color.fromARGB(255, 255, 181, 62),
+      Color.fromARGB(255, 255, 207, 147),
+    ],
+  );
---
+++ lib/utils/theme/widget_themes/outlined_button_theme.dart
@@ -0,0 +1,34 @@
+import 'package:blogify/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+
+
+/* -- Light & Dark Outlined Button Themes -- */
+class JOutlinedButtonTheme {
+  JOutlinedButtonTheme._(); //To avoid creating instances
+
+
+  /* -- Light Theme -- */
+  static final lightOutlinedButtonTheme  = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: JColor.dark,
+      side: const BorderSide(color: JColor.borderPrimary),
+      textStyle: const TextStyle(fontSize: 16, color: JColor.black, fontWeight: FontWeight.w600),
+      padding: const EdgeInsets.symmetric(vertical: JSize.buttonHeight, horizontal: 20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(JSize.buttonRadius)),
+    ),
+  );
+
+  /* -- Dark Theme -- */
+  static final darkOutlinedButtonTheme = OutlinedButtonThemeData(
+    style: OutlinedButton.styleFrom(
+      foregroundColor: JColor.light,
+      side: const BorderSide(color: JColor.borderPrimary),
+      textStyle: const TextStyle(fontSize: 16, color: JColor.textWhite, fontWeight: FontWeight.w600),
+      padding: const EdgeInsets.symmetric(vertical: JSize.buttonHeight, horizontal: 20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(JSize.buttonRadius)),
+    ),
+  );
+}
